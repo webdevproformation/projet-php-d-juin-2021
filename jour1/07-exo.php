aa3db89726ec71116d70a7974950541b8ab1b305
Malik H <malik.h@webdevpro.net>
2021-06-14 14:47:02 +0200

    install xdebug

--- jour1/07-exo.php
+++ jour1/07-exo.php
@@ -4,7 +4,7 @@ $homePage = [
     "http://domaine.fr/logo.jpg" ,
     "<h1>Titre principal</h1>" ,
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ex ac risus blandit laoreet. Vestibulum tincidunt ultricies quam in semper. Mauris nisl diam, mattis a euismod ut, ultricies et eros. Aliquam vitae lacinia elit. Phasellus nec consequat odio. Cras fringilla odio nec orci volutpat, quis dictum augue tempor. Vivamus in feugiat tellus, a aliquet neque. Ut sapien mi, blandit quis luctus sed, dapibus in lorem. Nullam hendrerit blandit elit, et placerat urna consectetur in."
- ];
+];
 $element1 = explode("/", $homePage[0]);
 
 $element2 = str_replace("<h1>", "", $homePage[1]); // enlevé la balise ouvrante
@@ -19,3 +19,5 @@ $homePage = [
 ] ;
 var_dump($homePage);
 // http://localhost/projet-php/jour1/07-exo.php
+
+// 08-xdebug.php
---
+++ jour1/08-xdebug.php
@@ -0,0 +1,26 @@
+<?php 
+
+// xdebug extension de PHP 
+// permet d'avoir des informations sur l'exécution du code en PHP 
+
+// avoir une information précise sur l'ensemble des variables à un moment donné dans le code
+
+// xampp ou MAMP disponible MAIS non installée par défaut 
+
+// pour l'installer il faut ajouter au fichier php.ini 
+
+phpinfo(); // fonction qui permet de connaître si une extension est bien disponible ??
+
+// http://localhost/projet-php/jour1/08-xdebug.php
+
+/*
+
+[xDebug]
+zend_extension = C:\xampp\php\ext\php_xdebug-2.9.5-7.4-vc15-x86_64.dll
+xdebug.remote_enable = 1
+xdebug.remote_autostart=on
+xdebug.var_display_max_depth=10
+
+la ligne avec écrit zend_extension va dépendre de l'ordinateur 
+
+*/
