e630a1c12506602276b5f2eac630a8a8b618480b
Malik H <malik.h@webdevpro.net>
2021-06-14 09:42:37 +0200

    balise php

---
+++ jour1/01-variable.php
@@ -0,0 +1,14 @@
+<?php 
+
+// commentaire en php 
+// tout code php commence par la balie ouvrant <?php 
+
+// si vous avez un fichier php qui ne contient QUE DU CODE PHP il est conseillé de ne pas mettre la balise fermante 
+
+// header("Location: "); => fonction du langage php qui permet de faire des redirections => il ne faut pas avoir un espace AVANT ce type de fonction 
+
+// aucun code html dedans 
+
+// si je suis amené à écrire du php et du html dans le même fichier alors il faudra fermer la balise php 
+
+
