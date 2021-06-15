7d7b54f5708f24867ae948008cc66bf9ee60d20a
Malik H <malik.h@webdevpro.net>
2021-06-15 13:52:57 +0200

    enoncé exo 7

--- jour2/06-this.php
+++ jour2/06-this.php
@@ -25,3 +25,22 @@ var_dump($f);
 var_dump($f->sommaire); // utilise à l'extérieur de la class directement dans un objet
 
 var_dump($f->description()); 
+
+// cas pratique :
+
+// créer le fichier 07-exo.php 
+
+// créer une class 
+// nom Personnage 
+// cette class dispose de 2 membres 
+// vie = 10 (attribut)
+// frapper ( autre_personnage ) 
+// lorsque le personnage va frapper ça va réduire de - 2 la vie de autre_personnage
+
+// créer un premier personnage magicien => instance de class Personnage
+// créer un deuxieme personnage guerrier => instance de class Personnage
+
+// le magicien va frapper 2 fois le guerrier
+// le guerrier va frapper 3 fois le magicien 
+
+// afficher les points de vie du guerrier et du magicien 
