23ee669a67cfd49600bd60d7d875ec404527eb92
Malik H <malik.h@webdevpro.net>
2021-06-18 12:37:29 +0200

    fin de la gestion des articles

--- tp/controller/admin.class.php
+++ tp/controller/admin.class.php
@@ -40,7 +40,6 @@ class AdminController extends Controller{
 
         }
         $this->render("ajouter");
-
     }
 
     public function suppr( $id = null ){
@@ -50,6 +49,42 @@ class AdminController extends Controller{
         header("Location: ".WWW."admin/accueil"); // redirection vers la page d'accueil du site 
         // header("Location: http://localhost/projet-php/tp/admin/accueil");
         // WWW constante qui est disponible dans le fichier index.php 
+        // 10h52 bon café tout le monde !! @ toute suite !!
+    }
+
+    public function modif($id = null){
+
+        // récupérer l'article numéro 2
+        $article = Bdd::getPdo()->query("SELECT * FROM articles WHERE id = :id", ["id" => $id] );
+
+        // si l'article que l'on veut modifier n'existe pas 10000 / plus 1 
+        if(empty($article)){
+            App::erreur404();
+            die();
+        }
+
+        if(
+                !empty($_POST) && 
+                strlen($_POST["titre"]) > 0 && 
+                strlen($_POST["contenu"]) > 0 && 
+                is_numeric($_POST["id"])
+        ){
+            // réaliser la mise à jour en base de données ! 
+            $sql = "UPDATE articles SET titre = :titre , contenu = :contenu WHERE id = :id";
+            Bdd::getPdo()->query($sql , $_POST);
+            header("Location: ".WWW."admin/accueil");
+            die();
+        }
+
+        // si l'article que l' on veut modifier existe bien  => remplir un formulaire avec les données de l'article 
+        $data = [
+            "article" => current($article)
+        ];
+        $this->send($data);
+        $this->render("modification");
+
+       /*  var_dump($article); */
+
     }
 
 }
